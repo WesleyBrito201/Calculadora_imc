0653d89c131e1155ba0a4a7376cff83f548a61ef
WesleyBrito201 <wesley.brito201@outlook.com>
2022-05-28 22:06:26 -0300

    correção do if, else if e else.

--- imc.js
+++ imc.js
@@ -7,13 +7,32 @@ function imc() {
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome.value !== '' && altura !== '' && peso !== '') {
-        const valorIMC = (peso/(altura*altura)).toFixed(2);
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
         let classificacao = '';
-        resultado.textContent =valorIMC;
-    } else { 
-        resultado.textContent ='Preencha todos os campos!!!' 
+
+        
+        if (valorIMC < 18.5) {
+            classificacao = "abaixo do peso.";
+        } else if (valorIMC < 25) {
+            classificacao = "com peso ideal. Parabéns!!!";
+        } else if (valorIMC < 30) {
+            classificacao = "levemente acima do peso. Cuidado!"
+        } else if (valorIMC < 35) {
+            classificacao = "com obesidade grau -I."
+        } else if (valorIMC < 40) {
+            classificacao = "com obesidade grau -II."
+        } else {
+            classificacao = "com obesidade grau -III. Procure um medico!"
+        }
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você esta ${classificacao}`
+    }else {
+        resultado.textContent ="Preencha todos os campos!!!";
     }
+
+
 }
 
 calcular.addEventListener('click', imc);
